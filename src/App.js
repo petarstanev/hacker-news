24c70e73b1fe03b95e2dfa8f28d781edd8371ec0
Petar Stanev <petarstanev@outlook.com>
2023-04-01 15:18:36 +0700

    Add icons to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import StoriesProvider from "./store/stories-provider";
 import Stories from "./components/Stories";
+import Header from "./components/Header";
 
 function App() {
   return (
     <StoriesProvider>
+      <Header />
       <Stories />
     </StoriesProvider>
   );
